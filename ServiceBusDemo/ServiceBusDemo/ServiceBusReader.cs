2485fbd03bb86f6cdd6e2e349ae1bcbee661108c
Andrew Poland <ajp159@live.com>
2018-05-21 19:47:41 -0700

    started reading from byte[] and logging details

--- ServiceBusDemo/ServiceBusDemo/ServiceBusReader.cs
+++ ServiceBusDemo/ServiceBusDemo/ServiceBusReader.cs
@@ -1,15 +1,38 @@
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.InteropExtensions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ServiceBusDemo
 {
     public static class ServiceBusReader
     {
         [FunctionName("ServiceBusReader")]
-        public static void Run([ServiceBusTrigger("NewSubscriptions", "SendSignupEmail", Connection = "ServiceBusRoot")]string mySbMsg, ILogger log)
+        public static void Run([ServiceBusTrigger("NewSubscriptions", "SendSignupEmail", Connection = "ServiceBusRootConnectionString")] byte[] structuredMessage, ILogger log)
         {
-            log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            SignupInformation newAccount = DeserializeJsonMessage<SignupInformation>(structuredMessage);
+            if (newAccount.AccountExists)
+            {
+                log.LogInformation("fix the filter on your subscription to prevent this.");
+            }
+
+            log.LogInformation($"C# ServiceBus topic trigger function processed message with email: {newAccount.EmailAddress}");
         }
+
+        private static T DeserializeJsonMessage<T>(byte[] message)
+        {
+            string jsonContent = Encoding.UTF8.GetString(message);
+            return JsonConvert.DeserializeObject<T>(jsonContent);
+        } 
+    }
+
+    public class SignupInformation
+    {
+        public bool AccountExists { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
+
