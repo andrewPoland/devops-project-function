35512940e27be7869ae0ed03a2c4c6101a8aa011
Andrew Poland <ajp159@live.com>
2018-07-22 11:34:26 -0700

    Added new output which is a map of all outputs.

--- DeploymentConfiguration/outputs.tf
+++ DeploymentConfiguration/outputs.tf
@@ -7,3 +7,13 @@ output "application_insights_instrumentation_key" {
   sensitive = true
   value = "${module.application_insights.instrumentation_key}"
 }
+
+output "all_for_one" {
+  sensitive = true
+  value = "${
+    map(
+      "topic_connection_string", "${module.service_bus.topic_connection_string}",
+      "application_insights_instrumentation_key", "${module.application_insights.instrumentation_key}"
+    )
+  }"
+}
